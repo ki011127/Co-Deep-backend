46e2bf030ec9e5d5a2eee08cf26ee5767860119c
ki011127 <ki011127@naver.com>
2024-07-04 15:12:00 +0900

    modify age convert

--- app/controller/episode_controller.py
+++ app/controller/episode_controller.py
@@ -5,8 +5,15 @@ from zoneinfo import ZoneInfo
 
 class EpisodeController:    
     async def get_episodes(self, story_id, age):
+        ver = 0
+        if age < 14:
+            ver = 1
+        elif age < 17:
+            ver = 2
+        else:
+            ver = 3
         print(story_id)
-        episodes = await mongodb.db.episodes.find({"story_id": story_id, "age": age},
+        episodes = await mongodb.db.episodes.find({"story_id": story_id, "age": ver},
             {"_id": 1, "story_id": 1, "age": 1, "order": 1, "title": 1, "clue_ids": 1, "img": 1, "description": 1}
         ).to_list(length=None)
         for i in range(len(episodes)):
